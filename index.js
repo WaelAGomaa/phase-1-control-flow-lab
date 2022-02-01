95a7189da574a038a2cc414ba3b4e09c8d1d6a95
wael <waeelash155@gmail.com>
2022-02-01 00:07:34 -0500

    Not Done Yet

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+if (someValue <= 400) {
+  return "This one is on me!";
+
+  } else if (someValue >= 2000){
+    return "I will gladly take your thirty bucks.";
+  } 
+    else if (someValue > 2500){
+    return 'No can do.';
+  }
+}
+
+
+function ternaryCheckCity(NYC){
+   NYC === Pittsburgh ? 'No go.' : `No go.`
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  function switchOnCharmFromTip(generous){
+    switch(generous){
+     case 'generous':
+        return 'Thank you so much.'
+     case 'not as generous':
+          return 'Thank you.'
+    default:
+            return "Bye."
+        
+    }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
